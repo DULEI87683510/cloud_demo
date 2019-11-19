e846c3014393e9005d02fb52684159b40521a41c
DL <dulei@travelsky.com>
2019-11-19 19:12:31 +0800

    eureka提交

--- user_consumer/src/main/java/user/service/Uservice.java
+++ user_consumer/src/main/java/user/service/Uservice.java
@@ -1,7 +1,6 @@
 package user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
