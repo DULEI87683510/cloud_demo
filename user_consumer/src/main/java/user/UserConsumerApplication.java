e535ec821b6b7baf7a7f8cd2b8c68baf4fa84009
DL <dulei@travelsky.com>
2019-11-22 11:45:25 +0800

    eureka提交

--- user_consumer/src/main/java/user/UserConsumerApplication.java
+++ user_consumer/src/main/java/user/UserConsumerApplication.java
@@ -2,6 +2,7 @@ package user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @ClassName consumer
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserConsumerApplication.class, args);
--- user_consumer/src/main/java/user/service/Uservice.java
+++ user_consumer/src/main/java/user/service/Uservice.java
@@ -1,8 +1,13 @@
 package user.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  *@ClassName Uservice
@@ -15,8 +20,22 @@ import org.springframework.web.client.RestTemplate;
 public class Uservice {
     @Autowired
     private RestTemplate restTemplate;
+    /**
+     *  Eureka客户端，可以获取到服务实例信息
+     */
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     public String getUserById(Long id){
-     String user= restTemplate.getForObject("http://localhost:8080/user/"+id,String.class);
+        // 根据服务名称，获取服务实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("user_service");
+        if (instances.isEmpty()){
+            throw new RuntimeException("获取的服务实例为空");
+        }
+        ServiceInstance serviceInstance=  instances.get(0);
+        // 获取ip和端口信息
+        String baseUrl = "http://"+serviceInstance.getHost() + ":" + serviceInstance.getPort()+"/user/";
+        String user= restTemplate.getForObject(baseUrl+id,String.class);
         return user;
     }
 }
